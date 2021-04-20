2dd10004cea46588306fcfba00d66b2bf95f516c
Rafijohari <Rafijohari1812@gmail.com>
2021-04-20 10:35:37 +0700

    add findkecamatan dan findkota

--- src/Location.php
+++ src/Location.php
@@ -54,6 +54,8 @@ class Location
     
     }
 
+    
+
     //ambil semua kota
     public static function getkota()
     {
@@ -89,6 +91,43 @@ class Location
                 return $data['rajaongkir']['status']['description'];
             }
         }    
+    
+    }
+    //ambil kota berdasarkan provinsi
+    public static function findkota($id)
+    { 
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(    
+        CURLOPT_URL => config('services.rajaongkir.account_type') == 'pro' ? "https://pro.rajaongkir.com/api/city?province=".$id : "https://api.rajaongkir.com/".config('services.rajaongkir.account_type')."/city?province=".$id,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => array(
+            "key:".config('services.rajaongkir.api_key')
+        ),
+        ));
+    
+    
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+    
+        curl_close($curl);
+    
+        if ($err) {
+            return "cURL Error #:" . $err;
+        } else {
+            $data =  json_decode($response,true);
+        
+            if($data['rajaongkir']['status']['code'] == 200){
+                return $data['rajaongkir']['results'];
+            }else{
+                return $data['rajaongkir']['status']['description'];
+            }
+        }  
     
     }
 
@@ -127,6 +166,45 @@ class Location
                 return $data['rajaongkir']['status']['description'];
             }
         }    
+    
+    }
+
+
+    //ambil kecamatan berdasarkan kota
+    public static function findkecamatan($id)
+    { 
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(    
+        CURLOPT_URL => "https://pro.rajaongkir.com/api/subdistrict?city=".$id,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => array(
+            "key:".config('services.rajaongkir.api_key')
+        ),
+        ));
+    
+    
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+    
+        curl_close($curl);
+    
+        if ($err) {
+            return "cURL Error #:" . $err;
+        } else {
+            $data =  json_decode($response,true);
+        
+            if($data['rajaongkir']['status']['code'] == 200){
+                return $data['rajaongkir']['results'];
+            }else{
+                return $data['rajaongkir']['status']['description'];
+            }
+        }  
     
     }
  
